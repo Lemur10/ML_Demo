162af85b5a56bb432d0d227e7aa46ee3c9f53e96
Lemur10 <150491945+Lemur10@users.noreply.github.com>
2023-11-21 18:22:09 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,7 @@ import streamlit as st
 import plotly.express as px
 from pycaret.regression import setup, compare_models, pull, save_model, load_model
 import pandas as pd
-
-from dataprep.eda import create_report
+import create_report
 import os 
 
 if os.path.exists('./dataset.csv'): 
