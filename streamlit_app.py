0e6eb55520db7f9a3b611779a65a8b1c73de84ad
Lemur10 <150491945+Lemur10@users.noreply.github.com>
2023-11-21 16:51:21 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import streamlit as st
 import plotly.express as px
 from pycaret.regression import setup, compare_models, pull, save_model, load_model
 import pandas as pd
-from streamlit_pandas_profiling import st_profile_report
+import st.pandas_profiling
+from st.pandas_profiling import st_profile_report
 import os 
 
 if os.path.exists('./dataset.csv'): 
