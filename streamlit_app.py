87e3c12028600679a0cbcdca18ef597606a05b40
Lemur10 <150491945+Lemur10@users.noreply.github.com>
2023-11-21 16:46:26 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ from operator import index
 import streamlit as st
 import plotly.express as px
 from pycaret.regression import setup, compare_models, pull, save_model, load_model
-from streamlit import pandas-profiling
 import pandas as pd
+import pandas-profiling
 from streamlit_pandas_profiling import st_profile_report
 import os 
 
