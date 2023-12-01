06496f00d39c222ffcb2681fcc98191c03e95ce5
Lemur10 <150491945+Lemur10@users.noreply.github.com>
2023-12-01 13:06:12 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,19 +24,21 @@ if choice == "Upload":
         df = pd.read_csv(file, index_col=None)
         df.to_csv('dataset.csv', index=None)
 
-
 if choice == "Profiling": 
     st.title("Exploratory Data Analysis")
     profile_df = df.profile_report()
     st_profile_report(profile_df)
-    
 
 if choice == "Modelling": 
     chosen_target = st.selectbox('Choose the Target Column', df.columns)
     if st.button('Run Modelling'): 
-        setup(df, target=chosen_target)
-        setup_df=st.dataframe(df["dataframe_column"].astype(int))
-
+        setup(df, target=chosen_target, silent=True)
+        setup_df = pull()
+        st.dataframe(setup_df)
+        best_model = compare_models()
+        compare_df = pull()
+        st.dataframe(compare_df)
+        save_model(best_model, 'best_model')
 
 if choice == "Download": 
     with open('best_model.pkl', 'rb') as f: 
