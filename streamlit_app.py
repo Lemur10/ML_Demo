1f1bb14b64cf96123f585e6505d30164967251ea
Lemur10 <150491945+Lemur10@users.noreply.github.com>
2023-12-01 12:28:16 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ with st.sidebar:
     st.info("This project application helps you explore your data.  As it is Auto generated, it is a very cursory look at things.")
 
 if choice == "Upload":
-    st.title("Upload Your Dataset")
+    st.title("To profile data, first Upload Your Dataset")
     file = st.file_uploader("Upload Your Dataset")
     if file: 
         df = pd.read_csv(file, index_col=None)
